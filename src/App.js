e3187e888a66216df9e8c2a43deeb3388cec619b
Athif Binu <binuathif@gmail.com>
2023-04-24 17:55:09 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import Header from './Components/Header/Header'
-import ProductList from './Components/ProductList/ProductList.jsx'
-
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <ProductList/>
+      
     </div>
   )
 }
---
+++ src/redux/Contands/ActionTypes.jsx
@@ -0,0 +1,6 @@
+export const ActionTypes={
+    SET_PRODUCTS:"set_products",
+    SELECTED_PRODUCT:"selected_product",
+    REMOVE_SELECTED_PRODUCT:"remove_selected_product",
+
+}
